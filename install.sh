8dfc50a6328971591dd444de5e81d8747c25dec0
Mahmoud Khawaja <mahmoud.khawaja97@gmail.com>
2025-04-20 05:24:45 +0200

    created installation script

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+
+mkdir -p build
+cd build
+
+
+cmake ..
+
+
+make
+
+
+sudo make install
+
+echo "gitz has been installed to /usr/local/bin"
+echo "you can now use it by running 'gitz <command> [<args>]'"
+echo "happy hawktuah nigga!"
