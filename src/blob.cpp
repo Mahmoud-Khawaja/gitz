7da5963df2c36264d4cb4c72152f36f80387fac3
Mahmoud Khawaja <mahmoud.khawaja97@gmail.com>
2025-04-20 19:10:26 +0200

    implemented the bolb

---
+++ src/blob.cpp
@@ -0,0 +1,19 @@
+#include "blob.h"
+
+namespace gitz {
+
+Blob::Blob(const std::string& content) : content_(content) {}
+
+std::string Blob::serialize() const {
+    return content_;
+}
+
+std::string Blob::getType() const {
+    return "blob";
+}
+
+std::string Blob::getContent() const {
+    return content_;
+}
+
+}  
